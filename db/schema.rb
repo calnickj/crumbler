55752035b9dc3eddadbca8dc6260a668d9dbb95c
Janeille Calnick <calnickj@gmail.com>
2016-05-15 01:11:13 -0400

    Sixth Commit: Added Images
    Added Images field to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501025034) do
+ActiveRecord::Schema.define(version: 20160515040332) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
